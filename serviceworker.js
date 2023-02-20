084553467c5038651f333c1ebbd1cdb7693ddfb7
Ben <reimerbe@shernet.sheridancollege.ca>
2023-02-20 14:37:29 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,110 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./serviceworker.js");
- }
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register("./serviceworker.js");
+//  }
+
+// const urlsToCache = ["/", "app.js", "styles.css", "logo.svg"];
+
+// self.addEventListener("install", event => {
+//    event.waitUntil(
+//       caches.open("pwa-assets").then(cache => {
+//          return cache.addAll(urlsToCache);
+//       });
+//    );
+// });
+
+
+
+
+
+
+var VERSION = 'v1';
+
+var cacheFirstFiles = [
+  // ADDME: Add paths and URLs to pull from cache first if it has been loaded before. Else fetch from network.
+  // If loading from cache, fetch from network in the background to update the resource. Examples:
+  "./index.html",
+  "./manifest.json",
+  "./images/icons-192.png",
+  "./images/icons-512.png",
+  "./images/icons-vector.svg"
+];
+
+var networkFirstFiles = [
+  // ADDME: Add paths and URLs to pull from network first. Else fall back to cache if offline. Examples:
+  // 'index.html',
+  // 'build/build.js',
+  // 'css/index.css'
+];
+
+// Below is the service worker code.
+
+var cacheFiles = cacheFirstFiles.concat(networkFirstFiles);
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+
+  console.log("fetching : ", event.request.url);
+  
+  if (event.request.method !== 'GET') { return; }
+  if (networkFirstFiles.indexOf(event.request.url) !== -1) {
+    console.log("network first : ", event);
+    event.respondWith(networkElseCache(event));
+  } else if (cacheFirstFiles.indexOf(event.request.url) !== -1) {
+    console.log("cache first : ", event.request.url);
+    event.respondWith(cacheElseNetwork(event));
+  } else {
+    console.log("neither first : ", event.request.url);
+    event.respondWith(fetch(event.request));
+  }
+});
+
+
+
+
+// If cache else network.
+// For images and assets that are not critical to be fully up-to-date.
+// developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
+// #cache-falling-back-to-network
+function cacheElseNetwork (event) {
+  return caches.match(event.request).then(response => {
+    function fetchAndCache () {
+       return fetch(event.request).then(response => {
+        // Update cache.
+        caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      });
+    }
+
+    // If not exist in cache, fetch.
+    if (!response) { return fetchAndCache(); }
+
+    // If exists in cache, return from cache while updating cache in background.
+    fetchAndCache();
+    return response;
+  });
+}
+
+// If network else cache.
+// For assets we prefer to be up-to-date (i.e., JavaScript file).
+function networkElseCache (event) {
+  return caches.match(event.request).then(match => {
+    if (!match) { return fetch(event.request); }
+    return fetch(event.request).then(response => {
+      // Update cache.
+      console.log("fetch");
+      caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }) || response;
+  });
+}
